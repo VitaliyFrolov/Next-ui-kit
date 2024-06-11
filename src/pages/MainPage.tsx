7716691cc832362b262b005000aa47fb41c07c15
Vitaliy <vitaliy.frolov.dev@gmail.com>
2024-06-11 11:28:25 +0300

    fix(components): changed folder architecture

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,10 @@
 'use client'
 import { Form, formData } from '@/components/features/Form';
-import { Slider } from '@/components/ui/Slider';
-import { sliderItems } from '@/components/ui/Slider/data/sliderItems';
 
 export default function MainPage() {
     return (
         <div>
-            <Slider
-                data={sliderItems}
-                width='100%'
-                height='400px'
-            />
+            hello apps
         </div>
     )
 };
